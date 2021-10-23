6605a6dde210f3a425c586a1f70295942437c307
jibonghoshbd <jibonghosh1510@gmail.comm>
2021-10-23 21:16:07 +0600

    first commit

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import { useRef } from "react";
+
+
 
 const AddUser = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const handelAddUser = e => {
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+
+        const newUser = { name, email };
+        fetch('http://localhost:5000/users ', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('Successfully Added User')
+                }
+            })
+        nameRef.current.value = '';
+        emailRef.current.value = '';
+        e.preventDefault();
+    }
+
     return (
         <div>
-            <h2>This is Add User</h2>
+            <h2>Please Add Users</h2>
+            <form onSubmit={handelAddUser}>
+                <input ref={nameRef} type="text" name="name" placeholder="Enter Your Name" id="" /> <br />
+                <input ref={emailRef} type="email" name="email" placeholder="Enter Your Email" id="" /> <br />
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     );
 };
